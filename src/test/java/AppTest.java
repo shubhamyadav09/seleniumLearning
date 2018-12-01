5ce13259e7168d30a89e2b9f2f08cc8a8036e389
shubhamyadav09 <shubhamyadav09@outlook.com>
2018-12-02 00:08:05 +0530

    added multiple drivers and testNG support

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,6 +1,9 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
 
 public class AppTest {
@@ -11,13 +14,31 @@ public class AppTest {
 
 	}
 	
+	@Test
 	public void login() {
 		final String baseURL = "https://katalon-demo-cura.herokuapp.com";
-		System.setProperty("webbriver.gecko.driver", "/SeleniumLearning/src/test/resources/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver", "src/test//resources//chromedriver.exe");
+		//WebDriver driver = new ChromeDriver();
+		
+		System.setProperty("webdriver.gecko.driver", "src/test//resources//geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		
+		//System.setProperty("webdriver.ie.driver", "src/test//resources//.exe" );
+		//WebDriver driver = new InternetExplorerDriver();
+		
+		//System.setProperty("webdriver.edge.driver", "src/test//resources//MicrosoftWebDriver.exe" );
+		//WebDriver driver = new EdgeDriver();
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		driver.get(baseURL);
 
 		driver.findElement(By.xpath("//*[@id=\"btn-make-appointment\"]")).click();
 		System.out.println(driver.getTitle());
+		driver.close();
 	}
 }
