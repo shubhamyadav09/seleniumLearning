e0596b8a62b25bf020826827184e205f1d1642df
shubhamyadav09 <shubhamyadav09@outlook.com>
2018-12-01 23:02:43 +0530

    updated test.java with selenium code

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,6 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class AppTest {
-	public AppTest() {
-		System.out.println("This is coming from app test class constructor");
-	} 
+
+	public static void main(String[] args) {
+		AppTest obj = new AppTest();
+		obj.login();
+
+	}
+	
+	public void login() {
+		final String baseURL = "https://katalon-demo-cura.herokuapp.com";
+		System.setProperty("webbriver.gecko.driver", "/SeleniumLearning/src/test/resources/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get(baseURL);
+
+		driver.findElement(By.xpath("//*[@id=\"btn-make-appointment\"]")).click();
+		System.out.println(driver.getTitle());
+	}
 }
